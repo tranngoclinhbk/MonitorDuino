c7382efe391264c778d162c7b1e2260c303cf793
tranngoclinhbk <tranngoclinh.bk@gmail.com>
2019-10-23 03:38:24 +0700

    Add files via upload

---
+++ HardwareMonitor/fmrMain.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace HardwareMonitor
+{
+    partial class fmrMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmrMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtTimer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbbCom = new System.Windows.Forms.ComboBox();
+            this.btnConn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.sentTb = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.txtTimer);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(406, 156);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cài đặt";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(364, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(302, 19);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(84, 32);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtTimer
+            // 
+            this.txtTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimer.Location = new System.Drawing.Point(187, 20);
+            this.txtTimer.Name = "txtTimer";
+            this.txtTimer.Size = new System.Drawing.Size(42, 29);
+            this.txtTimer.TabIndex = 2;
+            this.txtTimer.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(235, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "giây";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Thời gian làm mới:";
+            // 
+            // cbbCom
+            // 
+            this.cbbCom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbCom.FormattingEnabled = true;
+            this.cbbCom.Location = new System.Drawing.Point(88, 178);
+            this.cbbCom.Name = "cbbCom";
+            this.cbbCom.Size = new System.Drawing.Size(115, 32);
+            this.cbbCom.TabIndex = 2;
+            this.cbbCom.SelectedIndexChanged += new System.EventHandler(this.cbbCom_SelectedIndexChanged);
+            // 
+            // btnConn
+            // 
+            this.btnConn.BackColor = System.Drawing.Color.Red;
+            this.btnConn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConn.Location = new System.Drawing.Point(227, 174);
+            this.btnConn.Name = "btnConn";
+            this.btnConn.Size = new System.Drawing.Size(171, 40);
+            this.btnConn.TabIndex = 3;
+            this.btnConn.Text = "Kết nối";
+            this.btnConn.UseVisualStyleBackColor = false;
+            this.btnConn.Click += new System.EventHandler(this.btnConn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Cổng";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // sentTb
+            // 
+            this.sentTb.Location = new System.Drawing.Point(32, 124);
+            this.sentTb.Name = "sentTb";
+            this.sentTb.Size = new System.Drawing.Size(364, 20);
+            this.sentTb.TabIndex = 4;
+            this.sentTb.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // fmrMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 226);
+            this.Controls.Add(this.sentTb);
+            this.Controls.Add(this.btnConn);
+            this.Controls.Add(this.cbbCom);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "fmrMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MonitorDuino";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTimer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbCom;
+        private System.Windows.Forms.Button btnConn;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox sentTb;
+    }
+}
+
---
+++ HardwareMonitor/fmrMain.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO.Ports;
+using System.Configuration;
+using System.Security.Principal;
+
+namespace HardwareMonitor
+{
+    public partial class fmrMain : Form
+    {
+        //Khai báo biến
+        ManagementScope scope = new ManagementScope(@"\\.\root\OpenHardwareMonitor");
+        ObjectQuery query;
+        ManagementObjectSearcher moSearch;
+        ManagementObjectCollection moCollection;
+        string a;
+
+
+        private static bool IsRunAsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+        public fmrMain()
+        {
+            InitializeComponent();
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            if (IsRunAsAdmin()) Console.WriteLine("admin");
+            scope.Connect();
+            serialPort1.BaudRate = 9600;
+        }
+
+        public string _getValue(string Id)
+        {
+            string str = "";
+            string strQuery = "SELECT Value FROM Sensor WHERE InstanceId = " + Id;
+            query = new ObjectQuery(strQuery);
+            moSearch = new ManagementObjectSearcher(scope, query);
+            moCollection = moSearch.Get();
+            foreach (ManagementObject mo in moCollection)
+            {
+                str = mo["Value"].ToString();
+                break;
+            }
+            return str;
+        }
+        public string getIntValue(string Id, int length)
+        {
+            int res = (int)float.Parse(_getValue(Id));
+            string result = res.ToString();
+            while (result.Length < length)
+            {
+                result = " " + result;
+            }
+            return result;
+        }
+        public string getFloatValue(string Id, int length)
+        {
+            float res = float.Parse(_getValue(Id));
+            string result = "";
+            result = res.ToString("0.0");
+
+            while (result.Length < length)
+            {
+                result = " " + result;
+            }
+            return result;
+        }
+        public void fillText()
+        {
+
+            //CPU TEMP
+            a = _getValue("3848") + ",";
+
+            //CPU Load
+            a += getIntValue("3843",3) + ",";
+
+            //CPU Clock
+            a += getIntValue("3849", 3) + ",";
+
+            //ram Load         
+            a += getIntValue("3859",3) + ",";
+
+            //Used ram
+            a += getFloatValue("3860",3) + ",";
+
+            //GPU Clock        
+            a += getIntValue("3865",4) + ",";
+
+            //GPU Mem Clock
+            a += getIntValue("3866",4) + ",";
+
+            //GPU load
+            a += getIntValue("3868",3) + ",";
+
+            //GPU Ram Used
+            float ram = float.Parse(_getValue("3873")) / 1024;
+            a += ram.ToString("0.0") + ",";
+
+            //GPU Ram total
+            ram =float.Parse(_getValue("3872"))/1024;
+            a += ram.ToString("0") + ",";
+
+            //GPU Ram load
+            a += getIntValue("3875", 3) + ",";
+
+            //GPU Temperature
+            a += getIntValue("3863",2) + "*";
+
+            //textBox1.Text = a;
+            textBox1.BackColor = Color.White;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbbCom.DataSource = SerialPort.GetPortNames();
+            txtTimer.Text = ConfigurationManager.AppSettings["timer"].ToString();
+            timer1.Interval = (int)(double.Parse(txtTimer.Text) * 1000);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                fillText();
+                sentTb.Text = a;
+                serialPort1.Write(a);
+            }
+            catch
+            {
+                Error("OpenHardwareMonitor chưa được khởi động!");
+
+            }
+        }
+
+        private void btnConn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text = "";
+                
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.PortName = cbbCom.Text;
+                    serialPort1.Open();
+                    timer1.Start();
+                    btnConn.Text = "Ngắt kết nối";
+                    btnConn.BackColor = Color.Blue;
+                }
+                else
+                {
+                    timer1.Stop();
+                    btnConn.Text = "Kết nối";
+                    btnConn.BackColor = Color.Red;
+                    serialPort1.Close();
+                    Error("Đang dừng");
+                }
+            }
+            catch (Exception ex)
+            {
+                //Error(ex.ToString());
+                Error("Đang ngắt kết nối hoặc có lỗi xảy ra!");
+            }
+        }
+        void Error(string str)
+        {
+            textBox1.Text = str;
+            timer1.Stop();
+            btnConn.Text = "Kết nối";
+            btnConn.BackColor = Color.Red;
+            serialPort1.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Configuration _config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                _config.AppSettings.Settings["timer"].Value = txtTimer.Text;
+                _config.Save();
+                ConfigurationManager.RefreshSection("appSettings");
+            }
+            catch
+            {
+                MessageBox.Show("Có lỗi xảy ra, không lưu được!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cbbCom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
